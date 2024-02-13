58c2586774d551d637596e2de35cf672061f26c3
Danilo Sampaio <daniloarpesam@gmail.com>
2024-02-13 13:12:16 -0300

    refac vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,39 +1,44 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "fullcycle-vpc"
-  }
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "${var.prefix}-vpc"
+    }
 }
 
 // This will show the availability zones
-# data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {}
 # output "az" {
 #   value = "${data.aws_availability_zones.available.names}"
 # }
 
-resource "aws_subnet" "new-subnet-1" {
- availability_zone = "us-east-1a"
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.0.0/24"
- tags = {
-   Name = "fullcycle-subnet-1"
- }
+// create the subnets
+resource "aws_subnet" "subnets" {
+    count = 2
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.${count.index}.0/24"
+    map_public_ip_on_launch = true
+    tags = {
+         Name = "${var.prefix}-subnet-${count.index}"
+    }
 }
 
-resource "aws_subnet" "new-subnet-1" {
- availability_zone = "us-east-1b"
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.0.0/24"
- tags = {
-   Name = "fullcycle-subnet-1"
- }
-}
 
-resource "aws_subnet" "new-subnet-1" {
- availability_zone = "us-east-1a"
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.0.0/24"
- tags = {
-   Name = "fullcycle-subnet-2"
- }
-}
+// Manually create subnets
+# resource "aws_subnet" "new-subnet-1" {
+#     availability_zone = "us-east-1b"
+#     vpc_id = aws_vpc.main.id
+#     cidr_block = "10.0.0.0/24"
+#     tags = {
+#         Name = "${var.prefix}-subnet-2"
+#     }
+# }
+
+# resource "aws_subnet" "new-subnet-1" {
+#     availability_zone = "us-east-1a"
+#     vpc_id = aws_vpc.main.id
+#     cidr_block = "10.0.0.0/24"
+#     tags = {
+#         Name = "fullcycle-subnet-2"
+#     }
+# }
