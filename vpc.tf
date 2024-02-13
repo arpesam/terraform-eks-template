7aa2f24f0883f01a5a9406543124ba04b2a01172
Danilo Sampaio <daniloarpesam@gmail.com>
2024-02-13 13:27:33 -0300

    add rtb and ipgw

--- vpc.tf
+++ vpc.tf
@@ -11,6 +11,28 @@ data "aws_availability_zones" "available" {}
 #   value = "${data.aws_availability_zones.available.names}"
 # }
 
+
+// create the apigateway
+resource "aws_internet_gateway" "new-igw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+      Name = "${var.prefix}-igw"
+    }
+}
+
+// create the route table
+resource "aws_route_table" "new-rtb" {
+    vpc_id = aws_vpc.main.id
+    route {
+        // everyone can access
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.new-igw.id
+    }
+    tags = {
+      Name = "${var.prefix}-rtb"
+    }
+}
+
 // create the subnets
 resource "aws_subnet" "subnets" {
     count = 2
@@ -23,6 +45,14 @@ resource "aws_subnet" "subnets" {
     }
 }
 
+
+// associtate the subnets with the route table
+resource "aws_route_table_association" "new-rtb-association" {
+    count = 2
+    route_table_id = aws_route_table.new-rtb.id
+    // the * is a placeholder
+    subnet_id = aws_subnet.subnets.*.id[count.index]
+}
 
 // Manually create subnets
 # resource "aws_subnet" "new-subnet-1" {
