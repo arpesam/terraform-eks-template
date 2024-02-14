820c96e3f5147a9803d19183ed83f089ad10f36e
Danilo Sampaio <daniloarpesam@gmail.com>
2024-02-14 14:51:52 -0300

    add eks cluster configs

---
+++ cluster.tf
@@ -0,0 +1,68 @@
+
+// The cluster should be protected against external access, thats we need the sg group
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+  egress {
+    // all ports are open
+    from_port = 0
+    to_port = 0
+    // -1 means all protocols
+    protocol = "-1"
+    // all ips have access
+    cidr_blocks = ["0.0.0.0/0"]
+    // all sites are accessible
+    prefix_list_ids = []
+  }
+  tags = {
+    Name = "${var.prefix}-sg"
+  }
+}
+
+// create role and attach to eks
+resource "aws_iam_role" "cluster" {
+  name = "${var.prefix}-${var.cluster_name}-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+  POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  role = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_cloudwatch_log_group" "log" {
+  name = "/aws/eks/${var.prefix}-${var.cluster_name}/cluster"
+  retention_in_days = var.log_retention_days
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name = "${var.prefix}-${var.cluster_name}"
+  role_arn = aws_iam_role.cluster.arn
+  enabled_cluster_log_types = ["api","audit"]
+  vpc_config {
+    subnet_ids = aws_subnet.subnets[*].id
+    security_group_ids = [aws_security_group.sg.id]
+  }
+  depends_on = [
+    aws_cloudwatch_log_group.log,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
+  ]
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,3 @@
 prefix = "fullcycle"
+cluster_name = "dan-eks"
+log_retention_days = 30
